6446dae18cfec28e1e81b139a3bee5fd627ca1f3
1158553513@qq.com <1158553513@qq.com>
2019-06-11 20:26:50 +0800

    Initial commit

---
+++ src/main/java/com/fyqz/FyqzServerZuulApplication.java
@@ -0,0 +1,53 @@
+package com.fyqz;
+
+import com.spring4all.swagger.EnableSwagger2Doc;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+@EnableSwagger2Doc
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableDiscoveryClient
+@SpringBootApplication
+public class FyqzServerZuulApplication {
+	public static void main(String[] args) {
+		System.out.println("================================================== 开始启动 zuul网关服务 =============================================================");
+		System.out.println("请在控制台指定zuul网关服务的端口号 —— [端口号随意指定，注意不要与本机端口号出现冲突即可]");
+		Scanner scanner = new Scanner(System.in);
+		String port = scanner.nextLine(); //让用户指定端口号
+		new SpringApplicationBuilder(FyqzServerZuulApplication.class).properties("server.port=" + port).run(args);//启动项目
+		System.out.println("================================================== zuul网关服务 启动成功 =============================================================");
+	}
+
+	@Component
+	@Primary
+	class DocumentationConfig implements SwaggerResourcesProvider {
+		@Override
+		public List<SwaggerResource> get() {
+			List resources = new ArrayList<>();
+			resources.add(swaggerResource("service-chess", "/fyqz-service-chess/v2/api-docs", "2.0"));
+			resources.add(swaggerResource("service-user", "/fyqz-service-user/v2/api-docs", "2.0"));
+			resources.add(swaggerResource("service-scenario", "/fyqz-service-scenario/v2/api-docs", "2.0"));
+			resources.add(swaggerResource("service-deduce", "/fyqz-service-deduce/v2/api-docs", "2.0"));
+			return resources;
+		}
+		private SwaggerResource swaggerResource(String name, String location, String version) {
+			SwaggerResource swaggerResource = new SwaggerResource();
+			swaggerResource.setName(name);
+			swaggerResource.setLocation(location);
+			swaggerResource.setSwaggerVersion(version);
+			return swaggerResource;
+		}
+	}
+}
---
+++ src/main/java/com/fyqz/filter/MyZuulFilter.java
@@ -0,0 +1,53 @@
+package com.fyqz.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * zuul网关过滤器
+ * zuul不仅只是路由，并且还能过滤，做一些安全验证。
+ * 可以通过shouldFilter()方法返回值为false，来标明过滤器是否起作用
+ */
+@Component
+public class MyZuulFilter extends ZuulFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(MyZuulFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+    @Override
+    public Object run() {
+        RequestContext currentContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = currentContext.getRequest();
+        String requestURI = request.getRequestURI();
+        if(requestURI.contains("api-docs")){
+         return null;
+        }
+        logger.info("send{} request to {}", request.getMethod(), request.getRequestURI().toCharArray());
+        String accessToken = request.getHeader("TOKEN");
+        if (StringUtils.isBlank(accessToken)) {
+            logger.warn("accessToken is empty");
+            currentContext.setSendZuulResponse(false);
+            currentContext.setResponseStatusCode(401);
+            return null;
+        }
+        logger.info("accessToken {}", accessToken);
+        return null;
+    }
+}
